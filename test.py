384ee6522c942e2a8f66d79403809e456e94e13c
Aman Verma <120732430+raysalfaa@users.noreply.github.com>
2025-02-28 09:23:03 +0530

    Update test.py

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
-print("Hello world);
+def cal(a,b):
+  if(a>b):
+    return a-b
+  elif(a<b):
+    return a+b
+  else:
+    return a*b
+if __name__ == "__main__":
+  res=cal(8,6)
+  print(res)
+  print("calculation done)
